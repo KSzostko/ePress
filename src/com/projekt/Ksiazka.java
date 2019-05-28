416af4784931613cf2b3b10e932265c48803c7b9
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-28 21:39:25 +0200

    added field and methods to Ksiazka class

--- src/com/projekt/Ksiazka.java
+++ src/com/projekt/Ksiazka.java
@@ -1,4 +1,14 @@
 package com.projekt;
 
-public class Ksiazka {
+public class Ksiazka extends PozycjaLiteracka {
+    private String gatunekLiteracki;
+
+    public Ksiazka(String tytul, double cena, String gatunekLiteracki) {
+        super(tytul, cena);
+        this.gatunekLiteracki = gatunekLiteracki;
+    }
+
+    public String getGatunekLiteracki() {
+        return gatunekLiteracki;
+    }
 }
