24108f5333ddc170fd1f007a46bdc27c66feecda
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-28 21:33:54 +0200

    added fields and methods to PozycjaLiteracka class

--- src/com/projekt/PozycjaLiteracka.java
+++ src/com/projekt/PozycjaLiteracka.java
@@ -1,4 +1,19 @@
 package com.projekt;
 
 public abstract class PozycjaLiteracka {
+    private String tytul;
+    private double cena;
+
+    public PozycjaLiteracka(String tytul, double cena) {
+        this.tytul = tytul;
+        this.cena = cena;
+    }
+
+    public String getTytul() {
+        return tytul;
+    }
+
+    public double getCena() {
+        return cena;
+    }
 }
