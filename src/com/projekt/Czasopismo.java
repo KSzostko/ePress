4c996a5aebd3664d4206d7fe68d91b35c6e45003
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-28 21:37:17 +0200

    added field and methods to Czasopismo class

--- src/com/projekt/Czasopismo.java
+++ src/com/projekt/Czasopismo.java
@@ -1,4 +1,14 @@
 package com.projekt;
 
-public class Czasopismo {
+public class Czasopismo extends PozycjaLiteracka {
+    private String okresWydania;
+
+    public Czasopismo(String tytul, double cena, String okresWydania) {
+        super(tytul, cena);
+        this.okresWydania = okresWydania;
+    }
+
+    public String getOkresWydania() {
+        return okresWydania;
+    }
 }
