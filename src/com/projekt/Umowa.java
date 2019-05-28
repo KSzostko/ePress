a6d94cd809338dd3f100c571d3dd79cb4518f9ca
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-28 21:44:38 +0200

    added fields and methods to Umowa class

--- src/com/projekt/Umowa.java
+++ src/com/projekt/Umowa.java
@@ -1,4 +1,27 @@
 package com.projekt;
 
 public abstract class Umowa {
+    private int wynagrodzenie;
+    private String termin;
+
+    public Umowa(int wynagrodzenie, String termin) {
+        this.wynagrodzenie = wynagrodzenie;
+        this.termin = termin;
+    }
+
+    public int getWynagrodzenie() {
+        return wynagrodzenie;
+    }
+
+    public String getTermin() {
+        return termin;
+    }
+
+    public void setWynagrodzenie(int wynagrodzenie) {
+        this.wynagrodzenie = wynagrodzenie;
+    }
+
+    public void setTermin(String termin) {
+        this.termin = termin;
+    }
 }
