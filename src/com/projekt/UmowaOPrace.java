aa164b783e35a2c4c98f2b6779e602ce33e9641b
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-29 11:11:32 +0200

    added field and metohds to UmowaOPrace class

--- src/com/projekt/UmowaOPrace.java
+++ src/com/projekt/UmowaOPrace.java
@@ -1,5 +1,18 @@
 package com.projekt;
 
-public class UmowaOPrace {
+public class UmowaOPrace extends Umowa {
+    private int okresPracy;
 
+    public UmowaOPrace(int wynagrodzenie, int okresPracy) {
+        super(wynagrodzenie);
+        this.okresPracy = okresPracy;
+    }
+
+    public int getOkresPracy() {
+        return okresPracy;
+    }
+
+    public void setOkresPracy(int okresPracy) {
+        this.okresPracy = okresPracy;
+    }
 }
