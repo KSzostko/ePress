0dc1e71d25041efd1ae9329ab65b45be3be14c21
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-29 11:44:53 +0200

    added fields and methods to ZlecenieDruku class

--- src/com/projekt/ZlecenieDruku.java
+++ src/com/projekt/ZlecenieDruku.java
@@ -1,4 +1,29 @@
 package com.projekt;
 
 public class ZlecenieDruku {
+    private PozycjaLiteracka zleconaPozycja;
+    private Autor autor;
+    private int iloscEgzemplarzy;
+
+    public ZlecenieDruku(PozycjaLiteracka zleconaPozycja, Autor autor, int iloscEgzemplarzy) {
+        this.zleconaPozycja = zleconaPozycja;
+        this.autor = autor;
+        this.iloscEgzemplarzy = iloscEgzemplarzy;
+    }
+
+    public PozycjaLiteracka getZleconaPozycja() {
+        return zleconaPozycja;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public int getIloscEgzemplarzy() {
+        return iloscEgzemplarzy;
+    }
+
+    public void setIloscEgzemplarzy(int iloscEgzemplarzy) {
+        this.iloscEgzemplarzy = iloscEgzemplarzy;
+    }
 }
