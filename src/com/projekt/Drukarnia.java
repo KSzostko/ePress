a188c510927e709d4eed26195805097ea1d2ce87
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-29 11:54:16 +0200

    added fields, constructor and getters to Drukarnia class

--- src/com/projekt/Drukarnia.java
+++ src/com/projekt/Drukarnia.java
@@ -1,4 +1,22 @@
 package com.projekt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Drukarnia {
+    private List<ZlecenieDruku> zlecenia;
+    private String typDrukarni;
+
+    public Drukarnia(String typDrukarni) {
+        this.typDrukarni = typDrukarni;
+        this.zlecenia = new ArrayList<>();
+    }
+
+    public List<ZlecenieDruku> getZlecenia() {
+        return zlecenia;
+    }
+
+    public String getTypDrukarni() {
+        return typDrukarni;
+    }
 }
