66416a5d424da53ee74acc68ed187543e0d4ce66
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-29 11:19:33 +0200

    added fields and constructor in Autor class

--- src/com/projekt/Autor.java
+++ src/com/projekt/Autor.java
@@ -1,4 +1,24 @@
 package com.projekt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Autor {
+    private String imie;
+    private String nazwisko;
+    private List<Umowa> umowy;
+
+    public Autor(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.umowy = new ArrayList<>();
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
 }
