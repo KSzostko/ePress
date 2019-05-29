b3b6637bbb5bd2b12651fb53c3ebf156c8dfa83a
Kuba Szóstko <kubaszostko@gmail.com>
2019-05-29 11:14:58 +0200

    added field and methods to UmowaODzielo class

--- src/com/projekt/UmowaODzielo.java
+++ src/com/projekt/UmowaODzielo.java
@@ -1,4 +1,14 @@
 package com.projekt;
 
-public class UmowaODzielo {
+public class UmowaODzielo extends Umowa {
+    private PozycjaLiteracka dzielo;
+
+    public UmowaODzielo(int wynagrodzenie, PozycjaLiteracka dzielo) {
+        super(wynagrodzenie);
+        this.dzielo = dzielo;
+    }
+
+    public String getDzielo() {
+        return dzielo.getTytul();
+    }
 }
