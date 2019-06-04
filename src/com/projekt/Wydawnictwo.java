d2c80a88a75da932a6391eb070e67e551159d5eb
Kuba Szóstko <kubaszostko@gmail.com>
2019-06-04 09:31:54 +0200

    added fields, constructor and getters to Wydawnictwo class

--- src/com/projekt/Wydawnictwo.java
+++ src/com/projekt/Wydawnictwo.java
@@ -1,4 +1,34 @@
 package com.projekt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Wydawnictwo {
+    private String nazwaWydawnictwa;
+    private List<Autor> dostepniAutorzy;
+    private List<Drukarnia> drukarnie;
+    private Sklep sklep;
+
+    public Wydawnictwo(String nazwaWydawnictwa, Sklep sklep) {
+        this.nazwaWydawnictwa = nazwaWydawnictwa;
+        this.dostepniAutorzy = new ArrayList<>();
+        this.drukarnie = new ArrayList<>();
+        this.sklep = sklep;
+    }
+
+    public String getNazwaWydawnictwa() {
+        return nazwaWydawnictwa;
+    }
+
+    public List<Autor> getDostepniAutorzy() {
+        return dostepniAutorzy;
+    }
+
+    public List<Drukarnia> getDrukarnie() {
+        return drukarnie;
+    }
+
+    public Sklep getSklep() {
+        return sklep;
+    }
 }
