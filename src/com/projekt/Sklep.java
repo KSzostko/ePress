641fc50e404fbe54c6a627ddb690a4c226eb4aeb
Kuba Szóstko <kubaszostko@gmail.com>
2019-06-04 09:02:56 +0200

    added fields, constructor and getters to Sklep class

--- src/com/projekt/Sklep.java
+++ src/com/projekt/Sklep.java
@@ -1,4 +1,22 @@
 package com.projekt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sklep {
+    protected String nazwa;
+    protected List<Zlecenie> dostepnePozycje;
+
+    public Sklep(String nazwa) {
+        this.nazwa = nazwa;
+        this.dostepnePozycje = new ArrayList<>();
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public List<Zlecenie> getDostepnePozycje() {
+        return dostepnePozycje;
+    }
 }
