dbf775bce49d6d52a11edc3556e1fbc28842f0d9
Kuba Szóstko <kubaszostko@gmail.com>
2019-06-16 17:56:13 +0200

    added one new objest of class Ksiazka and 4 object of class Umowa

--- src/com/projekt/Main.java
+++ src/com/projekt/Main.java
@@ -7,11 +7,17 @@ public class Main {
 
         PozycjaLiteracka ksiazka1 = new Ksiazka("Wiedzmin", 24.99, "fantasy");
         Ksiazka ksiazka2 = new Ksiazka("Polska w obiektywie", 40.00, "album");
+        Ksiazka ksiazka3 = new Ksiazka("Historia informatyki", 50.25, "edukacyjna");
         PozycjaLiteracka czasopismo1 = new Czasopismo("CD-Action", 19.99, "miesiac");
         Czasopismo czasopismo2 = new Czasopismo("Świat wiedzy", 28.50, "kwartal");
 
         Autor autor1 = new Autor("Jan", "Kowalski");
         Autor autor2 = new Autor("Filip", "Sasinowski");
         Autor autor3 = new Autor("Jakub", "Szostko");
+
+        Umowa umowa1 = new UmowaODzielo(2500, ksiazka1);
+        UmowaODzielo umowa2 = new UmowaODzielo(1800, ksiazka3);
+        UmowaOPrace umowa3 = new UmowaOPrace(3300, 365);
+        UmowaOPrace umowa4 = new UmowaOPrace(4000, 180);
     }
 }
