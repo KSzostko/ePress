eb167222bd3fae384577d4978a8d819b582a9628
Kuba Szóstko <kubaszostko@gmail.com>
2019-06-16 17:47:45 +0200

    added 4 objects of class Pozycja Literacka in main

--- src/com/projekt/Main.java
+++ src/com/projekt/Main.java
@@ -3,6 +3,11 @@ package com.projekt;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Testing if .gitignore works properly");
+        System.out.println("Witaj w wydawnictwie ePress!");
+
+        PozycjaLiteracka ksiazka1 = new Ksiazka("Wiedzmin", 24.99, "fantasy");
+        Ksiazka ksiazka2 = new Ksiazka("Polska w obiektywie", 40.00, "album");
+        PozycjaLiteracka czasopismo1 = new Czasopismo("CD-Action", 19.99, "miesiac");
+        Czasopismo czasopismo2 = new Czasopismo("Świat wiedzy", 28.50, "kwartal");
     }
 }
