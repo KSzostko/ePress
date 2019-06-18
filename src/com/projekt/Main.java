a1abbe535b0a31b15dec0b2968163dae52499264
Filip Sasinowski <filip.sasinowski@gmail.com>
2019-06-19 00:18:33 +0200

    in theory menu is working now, but it is misisng two pieces in Umowy area

--- src/com/projekt/Main.java
+++ src/com/projekt/Main.java
@@ -89,6 +89,7 @@ public class Main {
         String userInput1;
         String userInput2;
         String userInput3;
+        String userInput5;
         int userInput4;
         Boolean mainRunning = true;
         Boolean wydawnictwoRunning = true;
@@ -96,6 +97,9 @@ public class Main {
         Boolean zarzad2Running = true;
         Boolean zarzad3Running = true;
         Boolean zarzad4Running = true;
+        Boolean drukarniaLoop = true;
+        Boolean sklepLoop = true;
+        Boolean miniLoop = true;
         String eater;
         double tripleDouble;
 
@@ -179,12 +183,16 @@ public class Main {
                                                     System.out.println("Podaj wynagrodzenie");
                                                     userInput4 = keyboard.nextInt();
                                                     eater = keyboard.nextLine();
-                                                    System.out.println("Podaj pozycje na która zawierana jest umowa:");
+                                                    System.out.println("Podaj tytul pozycji na która zawierana jest umowa:");
                                                     userInput3 = keyboard.nextLine();
-                                                    //Umowa umowa1 = new UmowaODzielo(2500, findBook(userInput3);
-                                                    //funckja ktroa znajdzie i zwroci ksiazke po tytule
-                                                    //Autor autor1 = new Autor(userInput2,userInput1);
-                                                    //autor1.zawrzyjUmowe(umowa1);
+                                                    System.out.println("Podaj cene tej pozycji");
+                                                    tripleDouble = keyboard.nextDouble();
+                                                    eater = keyboard.nextLine();
+                                                    System.out.println("Podaj rodzaj pozycji");
+                                                    userInput5 = keyboard.nextLine();
+                                                    Umowa umowa1 = new UmowaODzielo(userInput4, new Ksiazka(userInput3,tripleDouble,userInput5));
+                                                    Autor autor1 = new Autor(userInput2,userInput1);
+                                                    autor1.zawrzyjUmowe(umowa1);
                                                     break;
                                                 case 2:
                                                     System.out.println("Podaj imie autora");
@@ -321,10 +329,207 @@ public class Main {
                     }
                     break;
                 case 2:
-                    System.out.println("opcja 2");
+                    drukarniaLoop = true;
+                    while(drukarniaLoop){
+                        System.out.println("1.Wydrukuj");
+                        System.out.println("2.Przeglad wydrukowanych");
+                        System.out.println("3.Wroc");
+                        userChoice = keyboard.nextInt();
+                        eater = keyboard.nextLine();
+                        switch (userChoice){
+                            case 1:
+                                System.out.println("1.Czasopismo");
+                                System.out.println("2.Ksiazke");
+                                userChoice = keyboard.nextInt();
+                                eater = keyboard.nextLine();
+                                switch (userChoice){
+                                    case 1:
+                                        System.out.println("Podaj imie autora");
+                                        userInput1 = keyboard.nextLine();
+                                        System.out.println("Podaj nazwisko autora");
+                                        userInput2 = keyboard.nextLine();
+                                        System.out.println("Podaj tytul pozycji literackiej");
+                                        userInput3 = keyboard.nextLine();
+                                        System.out.println("Podaj cene pozycji literackiej");
+                                        tripleDouble = keyboard.nextDouble();
+                                        eater = keyboard.nextLine();
+                                        System.out.println("Podaj czas wydania");
+                                        userInput5 = keyboard.nextLine();
+                                        System.out.println("Podaj ilosc egzemplarzy do wydruku");
+                                        userChoice = keyboard.nextInt();
+                                        eater = keyboard.nextLine();
+                                        Zlecenie zlecenie1 = new Zlecenie(new Czasopismo(userInput3,tripleDouble,userInput5),new Autor(userInput2,userInput1),userChoice);
+                                        System.out.println("Podaj nazwe drukarni");
+                                        userInput1 = keyboard.nextLine();
+                                        System.out.println("Podaj czego drukarnia nie moze wydrukowac(zostaw puste jesli moze wszystko)");
+                                        userInput2 = keyboard.nextLine();
+                                        Drukarnia drukarnia1 = new Drukarnia(userInput1,userInput2);
+                                        drukarnia1.wydrukuj(zlecenie1);
+                                        break;
+                                    case 2:
+                                        System.out.println("Podaj imie autora");
+                                        userInput1 = keyboard.nextLine();
+                                        System.out.println("Podaj nazwisko autora");
+                                        userInput2 = keyboard.nextLine();
+                                        System.out.println("Podaj tytul pozycji literackiej");
+                                        userInput3 = keyboard.nextLine();
+                                        System.out.println("Podaj cene pozycji literackiej");
+                                        tripleDouble = keyboard.nextDouble();
+                                        eater = keyboard.nextLine();
+                                        System.out.println("Podaj rodzaj ksiazki");
+                                        userInput5 = keyboard.nextLine();
+                                        System.out.println("Podaj ilosc egzemplarzy do wydruku");
+                                        userChoice = keyboard.nextInt();
+                                        eater = keyboard.nextLine();
+                                        zlecenie1 = new Zlecenie(new Ksiazka(userInput3,tripleDouble,userInput5),new Autor(userInput2,userInput1),userChoice);
+                                        System.out.println("Podaj nazwe drukarni");
+                                        userInput1 = keyboard.nextLine();
+                                        System.out.println("Podaj czego drukarnia nie moze wydrukowac(zostaw puste jesli moze wszystko)");
+                                        userInput2 = keyboard.nextLine();
+                                        drukarnia1 = new Drukarnia(userInput1,userInput2);
+                                        drukarnia1.wydrukuj(zlecenie1);
+                                        break;
+                                        default:
+                                            break;
+                                }
+
+                                break;
+                            case 2:
+                            //przeglad wydrukowanych materialow
+                                break;
+                            case 3:
+                                drukarniaLoop = false;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
                     break;
                 case 3:
-                    System.out.println("opcja 3");
+                    sklepLoop = true;
+                    while(sklepLoop){
+                        System.out.println("1.Przejrzyj asortyment");
+                        System.out.println("2.Sprzedaj");
+                        System.out.println("3.Zarządz dostawe towaru do sklepu");
+                        System.out.println("4.Wroc");
+                        userChoice = keyboard.nextInt();
+                        eater = keyboard.nextLine();
+                        switch (userChoice){
+                            case 1:
+                                sklep.przejrzyjOferty();
+                                break;
+                            case 2:
+
+                                System.out.println("1.Czasopismo");
+                                System.out.println("2.Ksiazke");
+                                userChoice = keyboard.nextInt();
+                                eater = keyboard.nextLine();
+                                switch (userChoice){
+                                    case 1:
+                                        System.out.println("Podaj imie autora");
+                                        userInput1 = keyboard.nextLine();
+                                        System.out.println("Podaj nazwisko autora");
+                                        userInput2 = keyboard.nextLine();
+                                        System.out.println("Podaj tytul pozycji literackiej");
+                                        userInput3 = keyboard.nextLine();
+                                        System.out.println("Podaj cene pozycji literackiej");
+                                        tripleDouble = keyboard.nextDouble();
+                                        eater = keyboard.nextLine();
+                                        System.out.println("Podaj czas wydania");
+                                        userInput5 = keyboard.nextLine();
+                                        System.out.println("Podaj ilosc egzemplarzy do wydruku");
+                                        userChoice = keyboard.nextInt();
+                                        eater = keyboard.nextLine();
+                                        Zlecenie zamowienie = new Zlecenie(new Czasopismo(userInput3,tripleDouble,userInput5),new Autor(userInput2,userInput1),userChoice);
+                                        sklep.sprzedaj(zamowienie);
+                                        break;
+                                    case 2:
+                                        System.out.println("Podaj imie autora");
+                                        userInput1 = keyboard.nextLine();
+                                        System.out.println("Podaj nazwisko autora");
+                                        userInput2 = keyboard.nextLine();
+                                        System.out.println("Podaj tytul pozycji literackiej");
+                                        userInput3 = keyboard.nextLine();
+                                        System.out.println("Podaj cene pozycji literackiej");
+                                        tripleDouble = keyboard.nextDouble();
+                                        eater = keyboard.nextLine();
+                                        System.out.println("Podaj rodzaj ksiazki");
+                                        userInput5 = keyboard.nextLine();
+                                        System.out.println("Podaj ilosc egzemplarzy do wydruku");
+                                        userChoice = keyboard.nextInt();
+                                        eater = keyboard.nextLine();
+                                        zamowienie = new Zlecenie(new Ksiazka(userInput3,tripleDouble,userInput5),new Autor(userInput2,userInput1),userChoice);
+                                        sklep.sprzedaj(zamowienie);
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                break;
+                            case 3:
+                                    System.out.println("1.Czasopismo");
+                                    System.out.println("2.Ksiazke");
+                                    userChoice = keyboard.nextInt();
+                                    eater = keyboard.nextLine();
+                                    switch (userChoice){
+                                        case 1:
+                                            System.out.println("Podaj imie autora");
+                                            userInput1 = keyboard.nextLine();
+                                            System.out.println("Podaj nazwisko autora");
+                                            userInput2 = keyboard.nextLine();
+                                            System.out.println("Podaj tytul pozycji literackiej");
+                                            userInput3 = keyboard.nextLine();
+                                            System.out.println("Podaj cene pozycji literackiej");
+                                            tripleDouble = keyboard.nextDouble();
+                                            eater = keyboard.nextLine();
+                                            System.out.println("Podaj czas wydania");
+                                            userInput5 = keyboard.nextLine();
+                                            System.out.println("Podaj ilosc egzemplarzy do wydruku");
+                                            userChoice = keyboard.nextInt();
+                                            eater = keyboard.nextLine();
+                                            Zlecenie asortymentDoSklepu = new Zlecenie(new Czasopismo(userInput3,tripleDouble,userInput5),new Autor(userInput2,userInput1),userChoice);
+                                            System.out.println("Podaj nazwe drukarni");
+                                            userInput1 = keyboard.nextLine();
+                                            System.out.println("Podaj czego drukarnia nie moze wydrukowac(zostaw puste jesli moze wszystko)");
+                                            userInput2 = keyboard.nextLine();
+                                            Drukarnia drukarnia1 = new Drukarnia(userInput1,userInput2);
+                                            ePress.przekazDoSklepu(asortymentDoSklepu, drukarnia1);
+                                            break;
+                                        case 2:
+                                            System.out.println("Podaj imie autora");
+                                            userInput1 = keyboard.nextLine();
+                                            System.out.println("Podaj nazwisko autora");
+                                            userInput2 = keyboard.nextLine();
+                                            System.out.println("Podaj tytul pozycji literackiej");
+                                            userInput3 = keyboard.nextLine();
+                                            System.out.println("Podaj cene pozycji literackiej");
+                                            tripleDouble = keyboard.nextDouble();
+                                            eater = keyboard.nextLine();
+                                            System.out.println("Podaj rodzaj ksiazki");
+                                            userInput5 = keyboard.nextLine();
+                                            System.out.println("Podaj ilosc egzemplarzy do wydruku");
+                                            userChoice = keyboard.nextInt();
+                                            eater = keyboard.nextLine();
+                                            asortymentDoSklepu = new Zlecenie(new Ksiazka(userInput3,tripleDouble,userInput5),new Autor(userInput2,userInput1),userChoice);
+                                            System.out.println("Podaj nazwe drukarni");
+                                            userInput1 = keyboard.nextLine();
+                                            System.out.println("Podaj czego drukarnia nie moze wydrukowac(zostaw puste jesli moze wszystko)");
+                                            userInput2 = keyboard.nextLine();
+                                            drukarnia1 = new Drukarnia(userInput1,userInput2);
+                                            ePress.przekazDoSklepu(asortymentDoSklepu, drukarnia1);
+                                            break;
+                                        default:
+                                            break;
+                                }
+
+                                break;
+                            case 4:
+                                sklepLoop = false;
+                                break;
+                                default:
+                                    break;
+                        }
+                    }
+
                     break;
                 case 4:
                     mainRunning = false;
@@ -337,3 +542,15 @@ public class Main {
         System.out.println("koniec programu");
     }
 }
+// przekazywanie zasobow do sklepu, sprawdzamy, czy zmniejsza się ilość egzemplarzy w drukarni
+//        // i zmniejsza w sklepie lub też dodaje się całkowicie nowa poyzcja w sklepie
+//        drukarnia1.przejrzyjZlecenia();
+//        Zlecenie asortymentDoSklepu = new Zlecenie(ksiazka1, autor1, 550);
+//        ePress.przekazDoSklepu(asortymentDoSklepu, drukarnia1);
+//        sklep.przejrzyjOferty();
+//        drukarnia1.przejrzyjZlecenia();
+//
+//        // sprzedajemy książkę i sprawdzamy, czy zmienia to ilość egzemplarzy w sklepie
+//        Zlecenie zamownienie = new Zlecenie(ksiazka1, autor1, 20);
+//        sklep.sprzedaj(zamownienie);
+//        sklep.przejrzyjOferty();
