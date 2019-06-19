2e47af3f3794b87d4664cbc3a7eabcc4ebf5cf95
Kuba Szóstko <kubaszostko@gmail.com>
2019-06-19 07:26:54 +0200

    added DrukarniaTest class for unit testing

---
+++ src/com/projekt/DrukarniaTest.java
@@ -0,0 +1,14 @@
+package com.projekt;
+
+import static org.junit.Assert.*;
+
+public class DrukarniaTest {
+
+    @org.junit.Test
+    public void wydrukuj() {
+    }
+
+    @org.junit.Test
+    public void wyslijDoSklepu() {
+    }
+}
