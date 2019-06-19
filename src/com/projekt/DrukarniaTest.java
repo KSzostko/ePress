7ecc1fcb23d9499181c51981e66b8152486cea92
Kuba Szóstko <kubaszostko@gmail.com>
2019-06-19 07:29:23 +0200

    checking if unit testing is working properly

--- src/com/projekt/DrukarniaTest.java
+++ src/com/projekt/DrukarniaTest.java
@@ -6,9 +6,11 @@ public class DrukarniaTest {
 
     @org.junit.Test
     public void wydrukuj() {
+        fail("This method is yet to be implemented");
     }
 
     @org.junit.Test
     public void wyslijDoSklepu() {
+        fail("This method is yet to be implemented");
     }
 }
